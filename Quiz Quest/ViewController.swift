a2c4d0a1f9fc52ebd4d0fef79a843844c7148860
basit-dev <bas677@icloud.com>
2023-07-08 18:15:15 +0100

    Modified rounded corners file and updated label text

--- Quiz Quest/ViewController.swift
+++ Quiz Quest/ViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
-
+@IBDesignable
 class ViewController: UIViewController {
 
 
+    
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
+    
+    var score  = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        questionLabel.layer.cornerRadius = 40
+        questionLabel.layer.masksToBounds = true
+        scoreLabel.text = "Total score: \(score)"
     }
 }
 
