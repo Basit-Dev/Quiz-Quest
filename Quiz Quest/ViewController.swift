7fb364ea6a4ee3c9042c40d19d00d37f48b6c297
Abdul Basit <bas677@icloud.com>
2023-07-10 21:49:23 +0100

    Added logic for buttons, check question length validation

--- Quiz Quest/ViewController.swift
+++ Quiz Quest/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var mainView: UIView!
-    
+    @IBOutlet weak var progressBar: UIProgressView!
     
     var score = 0
     var questionNumber = 0
     
     let questions = [
         ["A slug's blood is green.", "True"],
-        ["Approximately one quarter of human bones are in the feet.","True"],
+        ["Approximately one quarter of human bones are in the feet.","False"],
         [ "The total surface area of two human lungs is approximately 70 square metres.", "True"],
     ]
     
@@ -27,21 +27,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        Create rounded corners on main view window
         mainView.layer.cornerRadius = 20
         mainView.layer.masksToBounds = true
+        
+//        Create default score and question label on initial view load
         scoreLabel.text = "Total score: \(score)"
         questionLabel.text = String(questions[questionNumber][0])
     }
     
-    
-//    Check if answer is correct
-    @IBAction func trueBtn(_ sender: UIButton) {
-        if questions[questionNumber][1] == "True" {
-            score += 1
-            questionNumber += 1
-            scoreLabel.text = "Total score: \(score)"
-            questionLabel.text = String(questions[questionNumber][0])
+    func checkArrayLength()  {
+        if questionNumber > questions.count - 1  {
+            questionNumber = 0
+            score = 0
         }
+    }
+    func updateUI()  {
+        
+        //        Update question number
+        questionNumber += 1
+        
+        //        Check if we have reached the last question
+        checkArrayLength()
+        
+        //        Update questions and score label
+        questionLabel.text = String(questions[questionNumber][0])
+        scoreLabel.text = "Total score: \(score)"
+    }
+    
+    //    Check if answer is correct
+    @IBAction func AnswerButton(_ sender: UIButton) {
+        
+        //        Get button title
+        let answerSelected = sender.titleLabel
+        
+        if questions[questionNumber][1] == answerSelected!.text! {
+            
+        //        Update score when answer is correct
+            score += 1
+        }
+        
+        //        Update UI
+        updateUI()
     }
 }
 
