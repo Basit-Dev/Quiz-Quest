ecca2df777269729bd7812760156c8b1d6435539
Abdul Basit <bas677@icloud.com>
2023-07-10 20:06:01 +0100

    Created questions and question validations

--- Quiz Quest/ViewController.swift
+++ Quiz Quest/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var mainView: UIView!
     
-    var score  = 0
+    
+    var score = 0
+    var questionNumber = 0
+    
+    let questions = [
+        ["A slug's blood is green.", "True"],
+        ["Approximately one quarter of human bones are in the feet.","True"],
+        [ "The total surface area of two human lungs is approximately 70 square metres.", "True"],
+    ]
     
     
     override func viewDidLoad() {
@@ -22,6 +30,18 @@ class ViewController: UIViewController {
         mainView.layer.cornerRadius = 20
         mainView.layer.masksToBounds = true
         scoreLabel.text = "Total score: \(score)"
+        questionLabel.text = String(questions[questionNumber][0])
+    }
+    
+    
+//    Check if answer is correct
+    @IBAction func trueBtn(_ sender: UIButton) {
+        if questions[questionNumber][1] == "True" {
+            score += 1
+            questionNumber += 1
+            scoreLabel.text = "Total score: \(score)"
+            questionLabel.text = String(questions[questionNumber][0])
+        }
     }
 }
 
